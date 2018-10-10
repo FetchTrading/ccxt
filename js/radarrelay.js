d110501a63a29a5b4fc3247ab80292d82a3d29d6
Jack Cook <jgacook@gmail.com>
2018-10-10 10:20:03 -0700

    add dai quote only modifier to radar relay

--- js/radarrelay.js
+++ js/radarrelay.js
@@ -5,35 +5,36 @@ const StandardRelayerV2 = require('./base/StandardRelayerV2');
 module.exports = class radarrelay extends StandardRelayerV2 {
     describe() {
         return this.deepExtend(super.describe(), {
-            'id': 'radarrelay2',
-            'name': 'Radar Relay',
-            'countries': 'USA',
-            'version': undefined,
-            'userAgent': undefined,
-            'rateLimit': 500,
-            'urls': {
-                'logo': 'https://radarrelay.com/img/radar-logo-beta.svg',
-                'api': 'https://api.radarrelay.com/0x/v2',
-                'www': 'https://radarrelay.com',
-                'doc': [
+            id: 'radarrelay2',
+            name: 'Radar Relay',
+            countries: 'USA',
+            version: undefined,
+            userAgent: undefined,
+            rateLimit: 500,
+            urls: {
+                logo: 'https://radarrelay.com/img/radar-logo-beta.svg',
+                api: 'https://api.radarrelay.com/0x/v2',
+                www: 'https://radarrelay.com',
+                doc: [
                     'https://radarrelay.com/standard-relayer-api/',
                 ],
             },
-            'has': {
-                'createOrder': false,
-                'createMarketOrder': false,
-                'createLimitOrder': false,
-                'fetchBalance': false,
-                'fetchCurrencies': false,
-                'fetchL2OrderBook': false,
-                'fetchMarkets': true,
-                'fetchOrderBook': true,
-                'fetchTicker': false,
-                'fetchTrades': false,
-                'privateAPI': false,
-                'is0xProtocol': true,
-                'needsEthereumNodeEndpoint': true,
-                'ethIsWeth': true,
+            has: {
+                createOrder: false,
+                createMarketOrder: false,
+                createLimitOrder: false,
+                fetchBalance: false,
+                fetchCurrencies: false,
+                fetchL2OrderBook: false,
+                fetchMarkets: true,
+                fetchOrderBook: true,
+                fetchTicker: false,
+                fetchTrades: false,
+                privateAPI: false,
+                is0xProtocol: true,
+                needsEthereumNodeEndpoint: true,
+                ethIsWeth: true,
+                daiQuoteOnly: true,
             },
         });
     }
